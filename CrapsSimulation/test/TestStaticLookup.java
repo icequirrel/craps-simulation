25b5ec3642cd0ba94493b6197e90724b483d5685
icequirrel <nab73@cornell.edu>
2020-09-18 20:23:51 -0500

    added 2 JUnit 5 tests for no static overriding

--- CrapsSimulation/test/TestStaticLookup.java
+++ CrapsSimulation/test/TestStaticLookup.java
@@ -2,13 +2,31 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.princeton.cs.introcs.StdOut;
+
 class TestStaticLookup
 {
 
 	@Test
-	void test()
+	void test_static_methods_dont_override()
 	{
-		fail("Not yet implemented");
+		Die alias = new CrookedDie1();
+		//CrookedDie1 test2 = new CrookedDie1(); 
+		
+		assertEquals("Die.testStatic()",alias.testStatic(), "error for this");
+		
+		//StdOut.println(((CrookedDie1)alias).testStatic());
+	}
+
+	@Test
+	void test_downcasting_changes_static_lookup()
+	{
+		Die alias = new CrookedDie1();
+		//CrookedDie1 test2 = new CrookedDie1(); 
+		
+		assertEquals("CrookedDie1.testStatic()",((CrookedDie1)alias).testStatic(), "error for this");
+		
+		//StdOut.println(((CrookedDie1)alias).testStatic());
 	}
 
 }
